2743b2cf7fc2fce317c3b8649886bfe5e0bef897
Márton Balassi <balassi.marton@gmail.com>
2014-03-27 12:18:19 +0100

    CellInfo refactor

---
+++ src/main/java/eu/stratosphere/streaming/test/cellinfo/CellInfo.java
@@ -0,0 +1,40 @@
+/***********************************************************************************************************************
+ *
+ * Copyright (C) 2010-2014 by the Stratosphere project (http://stratosphere.eu)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ **********************************************************************************************************************/
+
+package eu.stratosphere.streaming.test.cellinfo;
+
+import eu.stratosphere.nephele.jobgraph.JobGraph;
+import eu.stratosphere.streaming.api.JobGraphBuilder;
+import eu.stratosphere.test.util.TestBase2;
+import eu.stratosphere.types.StringValue;
+
+public class CellInfo extends TestBase2 {
+
+  @Override
+  public JobGraph getJobGraph() {
+    JobGraphBuilder graphBuilder = new JobGraphBuilder("testGraph");
+    graphBuilder.setSource("infoSource", InfoSourceInvokable.class);
+    graphBuilder.setSource("querySource", QuerySourceInvokable.class);
+    graphBuilder.setTask("cellTask", CellTaskInvokable.class, 2);
+    graphBuilder.setSink("sink", CellSinkInvokable.class);
+    
+    graphBuilder.fieldsConnect("infoSource", "cellTask", 0, StringValue.class);
+    graphBuilder.fieldsConnect("querySource", "cellTask",0, StringValue.class);
+    graphBuilder.broadcastConnect("cellTask", "sink");
+
+    return graphBuilder.getJobGraph();
+  }
+
+}
---
+++ src/main/java/eu/stratosphere/streaming/test/cellinfo/InfoSourceInvokable.java
@@ -0,0 +1,44 @@
+/***********************************************************************************************************************
+ *
+ * Copyright (C) 2010-2014 by the Stratosphere project (http://stratosphere.eu)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ **********************************************************************************************************************/
+
+package eu.stratosphere.streaming.test.cellinfo;
+
+import eu.stratosphere.streaming.api.AtomRecord;
+import eu.stratosphere.streaming.api.StreamRecord;
+import eu.stratosphere.streaming.api.invokable.UserSourceInvokable;
+import eu.stratosphere.types.IntValue;
+import eu.stratosphere.types.LongValue;
+
+public class InfoSourceInvokable extends UserSourceInvokable {
+
+	@Override
+	public void invoke() throws Exception {
+		for (int i = 0; i < 10; i++) {
+			StreamRecord batch1 = new StreamRecord(2);
+			AtomRecord record1 = new AtomRecord(2);
+			record1.setField(0, new IntValue(5));
+			record1.setField(1, new LongValue(500));
+			
+			StreamRecord batch2 = new StreamRecord(2);
+			AtomRecord record2 = new AtomRecord(2);
+			record2.setField(0, new IntValue(4));
+			record2.setField(1, new LongValue(500));
+			
+			emit(batch1);
+			emit(batch2);
+		}
+	}
+
+}
