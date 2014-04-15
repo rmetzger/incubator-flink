a5fde206ea3d2ec7820e2233f4b3d25056eb2941
gyfora <gyula.fora@gmail.com>
2014-04-15 10:31:58 +0200

    fault tolerance tests

--- src/test/java/eu/stratosphere/streaming/faulttolerance/AtLeastOnceBufferTest.java
+++ src/test/java/eu/stratosphere/streaming/faulttolerance/AtLeastOnceBufferTest.java
@@ -27,25 +27,25 @@ public class AtLeastOnceBufferTest {
 
 	AtLeastOnceFaultToleranceBuffer buffer;
 	int[] numberOfChannels;
+
 	@Before
 	public void setUp() throws Exception {
-		
-		numberOfChannels = new int[]{1,2};
-		
+
+		numberOfChannels = new int[] { 1, 2 };
+
 		buffer = new AtLeastOnceFaultToleranceBuffer(numberOfChannels, "1");
-		
-		
+
 	}
 
 	@Test
 	public void testAddToAckCounter() {
-		
+
 		StreamRecord record1 = new StreamRecord(new StringValue("R1")).setId("1");
 
-		buffer.addToAckCounter(record1.getId());	
+		buffer.addToAckCounter(record1.getId());
 
-		assertEquals((Integer) 3,buffer.ackCounter.get(record1.getId()));
-		
+		assertEquals((Integer) 3, buffer.ackCounter.get(record1.getId()));
+
 	}
 
 	@Test
@@ -53,77 +53,74 @@ public class AtLeastOnceBufferTest {
 		StreamRecord record1 = new StreamRecord(new StringValue("R1")).setId("1");
 		StreamRecord record2 = new StreamRecord(new StringValue("R2")).setId("1");
 
-		String id=record1.getId();
-		
-		buffer.addToAckCounter(record1.getId());	
-		buffer.addToAckCounter(record2.getId());	
+		String id = record1.getId();
+
+		buffer.addToAckCounter(record1.getId());
+		buffer.addToAckCounter(record2.getId());
 
 		record1.setId("1");
-		
+
 		buffer.removeFromAckCounter(record2.getId());
-		
-		assertEquals((Integer) 3,buffer.ackCounter.get(id));
+
+		assertEquals((Integer) 3, buffer.ackCounter.get(id));
 		assertFalse(buffer.ackCounter.containsKey(record2.getId()));
-		
+
 	}
 
 	@Test
 	public void testAck() {
-//		fail("Not yet implemented");
+		// fail("Not yet implemented");
 	}
 
 	@Test
 	public void testFailChannel() {
-//		fail("Not yet implemented");
+		// fail("Not yet implemented");
 	}
 
 	@Test
 	public void testAtLeastOnceFaultToleranceBuffer() {
-//		fail("Not yet implemented");
+		// fail("Not yet implemented");
 	}
 
 	@Test
 	public void testFaultToleranceBuffer() {
-//		fail("Not yet implemented");
+		// fail("Not yet implemented");
 	}
 
 	@Test
 	public void testAdd() {
-		
+
 		StreamRecord record1 = new StreamRecord(new StringValue("R1")).setId("1");
-		String id1=record1.getId();
+		String id1 = record1.getId();
 		buffer.add(record1);
 
 		record1.setRecord(new StringValue("R1"));
 		record1.setId("1");
-		String id2=record1.getId();
-
+		String id2 = record1.getId();
 
 		buffer.add(record1);
-		
-		assertEquals((Integer) 3,buffer.ackCounter.get(id1));
-		assertEquals((Integer) 3,buffer.ackCounter.get(id2));
 
-		assertEquals(2 , buffer.ackCounter.size());
-		
-		
-		
+		assertEquals((Integer) 3, buffer.ackCounter.get(id1));
+		assertEquals((Integer) 3, buffer.ackCounter.get(id2));
+
+		assertEquals(2, buffer.ackCounter.size());
+
 	}
 
 	@Test
 	public void testFail() {
-//		fail("Not yet implemented");
+		// fail("Not yet implemented");
 	}
 
 	@Test
 	public void testAddTimestamp() {
-		
+
 		Long ctime = System.currentTimeMillis();
-		
+
 		buffer.addTimestamp("1");
-		
+
 		assertEquals(ctime, buffer.recordTimestamps.get("1"));
-		
+
 		assertTrue(buffer.recordsByTime.containsKey(ctime));
 		assertTrue(buffer.recordsByTime.get(ctime).contains("1"));
 
@@ -131,7 +128,7 @@ public class AtLeastOnceBufferTest {
 
 	@Test
 	public void testRemove() {
-//		fail("Not yet implemented");
+		// fail("Not yet implemented");
 	}
 
 }
