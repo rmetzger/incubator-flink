782503455c9ed264270bd23cbf9ea790bf2ab0e0
jfeher <feherj@gmail.com>
2014-06-25 14:19:20 +0200

    more test in MapTest

--- src/test/java/eu/stratosphere/streaming/api/MapTest.java
+++ src/test/java/eu/stratosphere/streaming/api/MapTest.java
@@ -20,7 +20,6 @@ import static org.junit.Assert.*;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.jblas.util.Random;
 import org.junit.Test;
 
 import eu.stratosphere.api.java.functions.MapFunction;
@@ -32,58 +31,77 @@ public class MapTest {
 	public static final class MySource extends SourceFunction<Tuple1<Integer>> {
 
 		@Override
-		public void invoke(Collector<Tuple1<Integer>> collector)
-				throws Exception {
-			for(int i=0; i<10; i++){
+		public void invoke(Collector<Tuple1<Integer>> collector) throws Exception {
+			for (int i = 0; i < 10; i++) {
 				collector.collect(new Tuple1<Integer>(i));
 			}
 		}
 	}
-	
+
 	public static final class MyFieldsSource extends SourceFunction<Tuple1<Integer>> {
 
 		@Override
-		public void invoke(Collector<Tuple1<Integer>> collector)
-				throws Exception {
-			for(int i=0; i<MAXSOURCE; i++){
+		public void invoke(Collector<Tuple1<Integer>> collector) throws Exception {
+			for (int i = 0; i < MAXSOURCE; i++) {
 				collector.collect(new Tuple1<Integer>(5));
 			}
 		}
 	}
 	
-	public static final class MyMap extends MapFunction<Tuple1<Integer>,Tuple1<Integer>> {
+	public static final class MyDiffFieldsSource extends SourceFunction<Tuple1<Integer>> {
 
 		@Override
-		public Tuple1<Integer> map(Tuple1<Integer> value) throws Exception {
-			// TODO Auto-generated method stub
-			return new Tuple1<Integer>(value.f0*value.f0);
+		public void invoke(Collector<Tuple1<Integer>> collector) throws Exception {
+			for (int i = 0; i < 9; i++) {
+				collector.collect(new Tuple1<Integer>(i));
+			}
 		}
 	}
-	
-	public static final class MyFieldsMap extends MapFunction<Tuple1<Integer>,Tuple1<Integer>> {
 
-		
-		private int counter=0;
+	public static final class MyMap extends MapFunction<Tuple1<Integer>, Tuple1<Integer>> {
 
 		@Override
 		public Tuple1<Integer> map(Tuple1<Integer> value) throws Exception {
-			// TODO Auto-generated method stub
+			return new Tuple1<Integer>(value.f0 * value.f0);
+		}
+	}
+
+	public static final class MyFieldsMap extends MapFunction<Tuple1<Integer>, Tuple1<Integer>> {
+
+		private int counter = 0;
+
+		@Override
+		public Tuple1<Integer> map(Tuple1<Integer> value) throws Exception {
 			counter++;
-			
-			if(counter==MAXSOURCE) allInOne=true;
-			return new Tuple1<Integer>(value.f0*value.f0);
+
+			if (counter == MAXSOURCE)
+				allInOne = true;
+			return new Tuple1<Integer>(value.f0 * value.f0);
 		}
 	}
 	
+	public static final class MyDiffFieldsMap extends MapFunction<Tuple1<Integer>, Tuple1<Integer>> {
+
+		private int counter = 0;
+
+		@Override
+		public Tuple1<Integer> map(Tuple1<Integer> value) throws Exception {
+			counter++;
+			System.out.println(counter);
+			if (counter > 3)
+				threeInAll = false;
+			return new Tuple1<Integer>(value.f0 * value.f0);
+		}
+	}
+
 	public static final class MySink extends SinkFunction<Tuple1<Integer>> {
 
 		@Override
 		public void invoke(Tuple1<Integer> tuple) {
 			result.add(tuple.f0);
-			//System.out.println("result " + tuple.f0);
 		}
 	}
-	
+
 	public static final class MyBroadcastSink extends SinkFunction<Tuple1<Integer>> {
 
 		@Override
@@ -91,7 +109,15 @@ public class MapTest {
 			broadcastResult++;
 		}
 	}
-	
+
+	public static final class MyShufflesSink extends SinkFunction<Tuple1<Integer>> {
+
+		@Override
+		public void invoke(Tuple1<Integer> tuple) {
+			shuffleResult++;
+		}
+	}
+
 	public static final class MyFieldsSink extends SinkFunction<Tuple1<Integer>> {
 
 		@Override
@@ -99,54 +125,83 @@ public class MapTest {
 			fieldsResult++;
 		}
 	}
+	
+	public static final class MyDiffFieldsSink extends SinkFunction<Tuple1<Integer>> {
+
+		@Override
+		public void invoke(Tuple1<Integer> tuple) {
+			diffFieldsResult++;
+		}
+	}
+	
+	public static final class MyGraphSink extends SinkFunction<Tuple1<Integer>> {
+
+		@Override
+		public void invoke(Tuple1<Integer> tuple) {
+			graphResult++;
+		}
+	}
 
 	private static List<Integer> expected = new ArrayList<Integer>();
 	private static List<Integer> result = new ArrayList<Integer>();
 	private static int broadcastResult = 0;
+	private static int shuffleResult = 0;
 	private static int fieldsResult = 0;
-	private static int fieldsCounter = 0;
+	private static int diffFieldsResult = 0;
+	private static int graphResult = 0;
 	private static final int PARALELISM = 1;
 	private static final int MAXSOURCE = 10;
-	private static boolean allInOne=false;
+	private static boolean allInOne = false;
+	private static boolean threeInAll = true;
 
-	private static void fillExpectedList(){
-		for(int i=0;i<10;i++){
-			expected.add(i*i);
-			//System.out.println("expected " + i*i);
+	private static void fillExpectedList() {
+		for (int i = 0; i < 10; i++) {
+			expected.add(i * i);
 		}
 	}
-	
+
 	@Test
-	public void test() throws Exception {
-		
+	public void mapTest() throws Exception {
+
 		StreamExecutionEnvironment context = new StreamExecutionEnvironment();
 
-		DataStream<Tuple1<Integer>> dataStream = context
-				.addSource(new MySource(), 1)
-				.map(new MyMap(), PARALELISM)
-				.addSink(new MySink());
+		DataStream<Tuple1<Integer>> dataStream = context.addSource(new MySource(), 1)
+				.map(new MyMap(), PARALELISM).addSink(new MySink());
 
 		context.execute();
-		
+
 		fillExpectedList();
 
 		assertTrue(expected.equals(result));
 	}
-	
+
 	@Test
-	public void broadcastTest() throws Exception {
+	public void broadcastSinkTest() throws Exception {
 		StreamExecutionEnvironment context = new StreamExecutionEnvironment();
 		DataStream<Tuple1<Integer>> dataStream = context
 				.addSource(new MySource(), 1)
 				.broadcast()
 				.map(new MyMap(), 3)
 				.addSink(new MyBroadcastSink());
-		
+
 		context.execute();
 		assertEquals(30, broadcastResult);
-		
+
 	}
-	
+
+	@Test
+	public void shuffleSinkTest() throws Exception {
+		StreamExecutionEnvironment context = new StreamExecutionEnvironment();
+		DataStream<Tuple1<Integer>> dataStream = context
+				.addSource(new MySource(), 1)
+				.map(new MyMap(), 3)
+				.addSink(new MyShufflesSink());
+
+		context.execute();
+		assertEquals(10, shuffleResult);
+
+	}
+
 	@Test
 	public void fieldsSinkTest() throws Exception {
 		StreamExecutionEnvironment context = new StreamExecutionEnvironment();
@@ -155,12 +210,12 @@ public class MapTest {
 				.partitionBy(0)
 				.map(new MyMap(), 3)
 				.addSink(new MyFieldsSink());
-		
+
 		context.execute();
 		assertEquals(10, fieldsResult);
-		
+
 	}
-	
+
 	@Test
 	public void fieldsMapTest() throws Exception {
 		StreamExecutionEnvironment context = new StreamExecutionEnvironment();
@@ -169,9 +224,39 @@ public class MapTest {
 				.partitionBy(0)
 				.map(new MyFieldsMap(), 3)
 				.addSink(new MyFieldsSink());
-		
+
 		context.execute();
 		assertTrue(allInOne);
+
+	}
+	
+	@Test
+	public void diffFieldsMapTest() throws Exception {
+		StreamExecutionEnvironment context = new StreamExecutionEnvironment();
+		DataStream<Tuple1<Integer>> dataStream = context
+				.addSource(new MyDiffFieldsSource(), 1)
+				.partitionBy(0)
+				.map(new MyDiffFieldsMap(), 3)
+				.addSink(new MyDiffFieldsSink());
+
+		context.execute();
+		assertTrue(threeInAll);
+		assertEquals(9, diffFieldsResult);
+
+	}
+
+	
+	@Test
+	public void graphTest() throws Exception {
+		StreamExecutionEnvironment context = new StreamExecutionEnvironment();
+		DataStream<Tuple1<Integer>> dataStream = context
+				.addSource(new MySource(), 2)
+				.partitionBy(0)
+				.map(new MyMap(), 3)
+				.addSink(new MyGraphSink());
+
+		context.execute();
+		assertEquals(20, graphResult);
 		
 	}
 }
