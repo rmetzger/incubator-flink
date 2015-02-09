697a10ce52d162847f5efc61755489e0d4da8e6d
Robert Metzger <rmetzger@apache.org>
2015-02-09 09:31:43 +0100

    wip

--- flink-runtime/src/main/resources/web-docs-infoserver/js/taskmanager.js
+++ flink-runtime/src/main/resources/web-docs-infoserver/js/taskmanager.js
@@ -48,6 +48,46 @@ var memoryValues = ["memory.total.used", "memory.heap.used", "memory.non-heap.us
 Create rickshaw graph for the specified taskManager id (tmid).
 **/
 function createGraph(tmId) {
+    var palette = new Rickshaw.Color.Palette({scheme: "spectrum14"} );
+    var series = [];
+    for(i in memoryValues) {
+        var value = memoryValues[i];
+        taskManagerMemory[tmId][value] = [];
+        series.push({
+            color: convertHex(palette.color(), 80),
+            data: taskManagerMemory[tmId][value],
+            name: value,
+            stroke: 'rgba(0,0,0,0.5)'
+        });
+    }
+    var graph = new Rickshaw.Graph( {
+            element: document.querySelector("#chart-"+tmId),
+            width: 580,
+            height: 250,
+            series: series,
+            renderer: 'area',
+            stroke: true
+    } );
+    graph.renderer.unstack = true;
+    var x_axis = new Rickshaw.Graph.Axis.Time( { graph: graph } );
+
+    var y_axis = new Rickshaw.Graph.Axis.Y( {
+            graph: graph,
+            orientation: 'left',
+            tickFormat: Rickshaw.Fixtures.Number.formatKMBT,
+            element: document.getElementById("y_axis-"+tmId),
+    } );
+
+    var hoverDetail = new Rickshaw.Graph.HoverDetail( {
+        graph: graph,
+        yFormatter: Rickshaw.Fixtures.Number.formatKMBT
+    } );
+
+    var legend = new Rickshaw.Graph.Legend({
+        graph: graph,
+        element: document.querySelector("#legend-"+tmId)
+    });
+    return graph;
 }
 /*
  * Initializes taskmanagers table
@@ -61,11 +101,11 @@ function processTMdata(json) {
 		// check if taskManager has a row
 		tmRow = $("#"+tmRowIdCssName)
 		if(tmRow.length == 0) {
-		    var tmMemoryBox = "<div class=\"chart_container\">"+
-                                  "<div class=\"y_axis\"></div>"+
-                                  "<div class=\"chart\"></div>"+
+		    var tmMemoryBox = "<div class=\"chart_container\" id=\"chart_container-"+tm.id+"\">"+
+                                  "<div class=\"y_axis\" id=\"y_axis-"+tm.id+"\"></div>"+
+                                  "<div class=\"chart\" id=\"chart-"+tm.id+"\"></div>"+
                                "</div>"+
-                               "<div class=\"legend\"></div>";
+                               "<div class=\"legend\" id=\"legend-"+tm.id+"\"></div>";
 		    // the taskamanger does not yet have a table row
 		    tableHeader.after("<tr id=\""+tmRowIdCssName+"\">" +
 		                "<td>"+tm.inetAdress+" <br> IPC Port: "+tm.ipcPort+", Data Port: "+tm.dataPort+"</td>" + // first row: TaskManager
@@ -74,14 +114,17 @@ function processTMdata(json) {
 		                "</tr>");
 		    taskManagerMemory[tm.id] = []; // create empty array for TM
 		    taskManagerGraph[tm.id] = createGraph(tm.id);
+		    taskManagerGraph[tm.id].render();
 		}
         // fill (update) row with contents
         // memory statistics
         var time = getUnixTime();
         for(i in memoryValues) {
             value = memoryValues[i];
-            taskManagerMemory[tm.id][value].push({x: time, y: data.gauges[value].value})
+            metricsJSON = $.parseJSON(tm.metrics);
+            taskManagerMemory[tm.id][value].push({x: time, y: metricsJSON.gauges[value].value})
         }
+        taskManagerGraph[tm.id].update();
 
 
         // info box
