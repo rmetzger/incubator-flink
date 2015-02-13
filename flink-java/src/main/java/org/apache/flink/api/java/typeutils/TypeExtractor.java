apache/flink

[FLINK-1391] Add support for using Avro-POJOs and Avro types with Kryo

flink-java/src/main/java/org/apache/flink/api/java/typeutils/TypeExtractor.java
@@ -30,6 +30,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import org.apache.avro.specific.SpecificRecordBase;
 import org.apache.commons.lang3.Validate;
 import org.apache.flink.api.common.functions.CoGroupFunction;
 import org.apache.flink.api.common.functions.CrossFunction;
@@ -71,7 +72,7 @@ public class TypeExtractor {
 	// in an endless recursion
 	private Set<Class<?>> alreadySeen;
 
-	private TypeExtractor() {
+	protected TypeExtractor() {
 		alreadySeen = new HashSet<Class<?>>();
 	}
 
@@ -1050,6 +1051,11 @@ public class TypeExtractor {
 			return (TypeInformation<X>) new EnumTypeInfo(clazz);
 		}
 
+		// special case for POJOs generated by Avro.
+		if(SpecificRecordBase.class.isAssignableFrom(clazz)) {
+			return (TypeInformation<X>) new AvroTypeInfo(clazz);
+		}
+
 		if (alreadySeen.contains(clazz)) {
 			return new GenericTypeInfo<X>(clazz);
 		}
@@ -1142,7 +1148,7 @@ public class TypeExtractor {
 	}
 
 	@SuppressWarnings("unchecked")
-	private <X> TypeInformation<X> analyzePojo(Class<X> clazz, ArrayList<Type> typeHierarchy, ParameterizedType clazzTypeHint) {
+	protected <X> TypeInformation<X> analyzePojo(Class<X> clazz, ArrayList<Type> typeHierarchy, ParameterizedType clazzTypeHint) {
 		// try to create Type hierarchy, if the incoming only contains the most bottom one or none.
 		if(typeHierarchy.size() <= 1) {
 			getTypeHierarchy(typeHierarchy, clazz, Object.class);

rmetzger/incubator-flink

Conflicts:
	flink-java/src/main/java/org/apache/flink/api/java/typeutils/TypeExtractor.java
	flink-java/src/main/java/org/apache/flink/api/java/typeutils/runtime/KryoSerializer.java

flink-java/src/main/java/org/apache/flink/api/java/typeutils/TypeExtractor.java
@@ -30,6 +30,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import org.apache.avro.specific.SpecificRecordBase;
 import org.apache.commons.lang3.Validate;
 import org.apache.flink.api.common.functions.CoGroupFunction;
 import org.apache.flink.api.common.functions.CrossFunction;
@@ -71,7 +72,7 @@ public class TypeExtractor {
 	// in an endless recursion
 	private Set<Class<?>> alreadySeen;
 
-	private TypeExtractor() {
+	protected TypeExtractor() {
 		alreadySeen = new HashSet<Class<?>>();
 	}
 
@@ -1154,6 +1155,11 @@ public class TypeExtractor {
 		// check for Enums
 		if(Enum.class.isAssignableFrom(clazz)) {
 			return (TypeInformation<OUT>) new EnumTypeInfo(clazz);
+		}
+
+		// special case for POJOs generated by Avro.
+		if(SpecificRecordBase.class.isAssignableFrom(clazz)) {
+			return (TypeInformation<OUT>) new AvroTypeInfo(clazz);
 		}
 
 		if (alreadySeen.contains(clazz)) {