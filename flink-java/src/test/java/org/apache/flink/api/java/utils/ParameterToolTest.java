67c6a69eeb86002011da532457c052e12801535a
Robert Metzger <rmetzger@apache.org>
2015-11-21 18:51:26 +0100

    wip

--- flink-java/src/test/java/org/apache/flink/api/java/utils/ParameterToolTest.java
+++ flink-java/src/test/java/org/apache/flink/api/java/utils/ParameterToolTest.java
@@ -154,11 +154,6 @@ public class ParameterToolTest {
 		validate(parameter);
 	}
 
-	@Test
-	public void testFromGenericOptionsParser() throws IOException {
-		ParameterTool parameter = ParameterTool.fromGenericOptionsParser(new String[]{"-D", "input=myInput", "-DexpectedCount=15"});
-		validate(parameter);
-	}
 
 	private void validate(ParameterTool parameter) {
 		ClosureCleaner.ensureSerializable(parameter);
