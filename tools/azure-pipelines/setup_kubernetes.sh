f0806e1e0979394808960190b8665127aedb4b24
Robert Metzger <rmetzger@apache.org>
2020-01-31 11:13:15 +0100

    potential fix for the e2e tests [ci skip]

--- tools/azure-pipelines/setup_kubernetes.sh
+++ tools/azure-pipelines/setup_kubernetes.sh
@@ -13,22 +13,34 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+echo "Replace moby by docker"
+docker version
+apt-get remove -y moby-engine
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+docker version
+
 
 # Download minikube.
-echo "Download minikube"
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.25.2/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
-sudo minikube start --vm-driver=none --kubernetes-version=v1.9.0
+# echo "Download minikube"
+# curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.25.2/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
+# sudo minikube start --vm-driver=none --kubernetes-version=v1.9.0
 
-echo "Move files and change permissions"
-sudo mv /root/.kube $HOME/.kube # this will write over any previous configuration
-sudo chown -R $USER $HOME/.kube
-sudo chgrp -R $USER $HOME/.kube
+# echo "Move files and change permissions"
+# sudo mv /root/.kube $HOME/.kube # this will write over any previous configuration
+# sudo chown -R $USER $HOME/.kube
+# sudo chgrp -R $USER $HOME/.kube
 
-sudo mv /root/.minikube $HOME/.minikube # this will write over any previous configuration
-sudo chown -R $USER $HOME/.minikube
-sudo chgrp -R $USER $HOME/.minikube 
+# sudo mv /root/.minikube $HOME/.minikube # this will write over any previous configuration
+# sudo chown -R $USER $HOME/.minikube
+# sudo chgrp -R $USER $HOME/.minikube 
 
-echo "Fix the kubectl context, as it's often stale."
-minikube update-context
-echo "Wait for Kubernetes to be up and ready."
-JSONPATH='{range .items[*]}{@.metadata.name}:{range @.status.conditions[*]}{@.type}={@.status};{end}{end}'; until kubectl get nodes -o jsonpath="$JSONPATH" 2>&1 | grep -q "Ready=True"; do sleep 1; done
+# echo "Fix the kubectl context, as it's often stale."
+# minikube update-context
+# echo "Wait for Kubernetes to be up and ready."
+# JSONPATH='{range .items[*]}{@.metadata.name}:{range @.status.conditions[*]}{@.type}={@.status};{end}{end}'; until kubectl get nodes -o jsonpath="$JSONPATH" 2>&1 | grep -q "Ready=True"; do sleep 1; done
