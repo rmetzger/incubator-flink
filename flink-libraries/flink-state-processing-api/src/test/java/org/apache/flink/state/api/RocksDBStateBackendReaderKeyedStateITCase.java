91cc7dc8201675089dadb52663f9c45ae5ea41e4
Robert Metzger <rmetzger@apache.org>
2020-03-05 21:55:59 +0100

    Ignore rocksdb test

--- flink-libraries/flink-state-processing-api/src/test/java/org/apache/flink/state/api/RocksDBStateBackendReaderKeyedStateITCase.java
+++ flink-libraries/flink-state-processing-api/src/test/java/org/apache/flink/state/api/RocksDBStateBackendReaderKeyedStateITCase.java
@@ -21,10 +21,12 @@ package org.apache.flink.state.api;
 import org.apache.flink.contrib.streaming.state.RocksDBStateBackend;
 import org.apache.flink.runtime.state.StateBackend;
 import org.apache.flink.runtime.state.memory.MemoryStateBackend;
+import org.junit.Ignore;
 
 /**
  * IT Case for reading state from a RocksDB keyed state backend.
  */
+@Ignore
 public class RocksDBStateBackendReaderKeyedStateITCase extends SavepointReaderKeyedStateITCase<RocksDBStateBackend> {
 	@Override
 	protected RocksDBStateBackend getStateBackend() {
