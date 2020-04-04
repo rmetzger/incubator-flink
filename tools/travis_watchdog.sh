92e83e35103b1566d12ed2a2cda40303b63d0442
Robert Metzger <rmetzger@apache.org>
2020-04-04 15:27:24 +0200

    distinguish better

--- tools/travis_watchdog.sh
+++ tools/travis_watchdog.sh
@@ -100,7 +100,9 @@ fi
 # enable coredumps
 ulimit -c unlimited
 JVM_CRASH_REPORTS_LOCATION="${ARTIFACTS_DIR}/jvm-crash-reports"
-export JAVA_TOOL_OPTIONS="-XX:ErrorFile=$JVM_CRASH_REPORTS_LOCATION -XX:HeapDumpPath=$JVM_CRASH_REPORTS_LOCATION -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=$JVM_CRASH_REPORTS_LOCATION"
+mkdir $JVM_CRASH_REPORTS_LOCATION/error-file
+mkdir $JVM_CRASH_REPORTS_LOCATION/heapdumps
+export JAVA_TOOL_OPTIONS="-XX:ErrorFile=$JVM_CRASH_REPORTS_LOCATION/error-file -XX:HeapDumpPath=$JVM_CRASH_REPORTS_LOCATION/heapdumps -XX:+HeapDumpOnOutOfMemoryError"
 
 if [ $TEST == $STAGE_PYTHON ]; then
 	CMD=$PYTHON_TEST
